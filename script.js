c8fa12b2a3746d51e4d1e12ea647d9842961f6bc
calher99 <calher99@gmail.com>
2022-10-27 14:38:41 +0100

    add render method to gameBoard and create factory function userFactory for user creation

--- script.js
+++ script.js
@@ -1,26 +1,69 @@
 const gameBoard =(function () {
 
-    let _board =[[0,0,0],[0,0,0],[0,0,0]];
-    
+    let _board =[['','',''],['','',''],['','','']];
+    const _htmlBoard = document.querySelectorAll('[data-row]')
+
     const initialize = () => {
         for(let i = 0 ; i < 3; i++ ){
             for(let j = 0 ; j < 3; j++ ){
                 _board[i][j] = '';
             }
         }
+        
     }
     const move = (row,column,token) =>{
         _board[row][column] = token;
 
     }
-    const print = () => console.table(_board);
+
+    const render = () => {
+        let k=0;
+        for(let i = 0 ; i < 3; i++ ){
+            for(let j = 0 ; j < 3; j++ ){
+                _htmlBoard[k].textContent= _board[i][j];
+                k++;
+            }
+        }
+    }
 
     return{
         initialize,
         move,
-        print
+        render
     }
 })()
 
+const userFactory = () => {
+    let _tokenType ='undefined';
+    let name = '';
+    const _htmlBoard = document.querySelectorAll('[data-row]')
+
+    const setToken = (token) =>{
+        _tokenType = token;
+    }
+    const playTurn = () =>{
 
 
+        gameBoard.move(row,col,_tokenType)
+    }
+
+    return{
+        setToken,
+    }
+
+
+}
+
+const displayController = (function(){
+    //checkWin
+    //giveTurn ?¿
+
+})()
+const playerOne = userFactory();
+const playerTwo = userFactory();
+
+
+gameBoard.initialize();
+gameBoard.render();
+gameBoard.move(1,1,"x");
+gameBoard.render();
